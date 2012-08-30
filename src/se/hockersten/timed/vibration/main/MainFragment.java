dbc7720bc7f7fa3f64e311c5b2f48838ff0f907a
Anders Höckersten <anders@hockersten.se>
2012-08-30 09:16:36 +0200

    App now handles rotation.

--- src/se/hockersten/timed/vibration/main/MainFragment.java
+++ src/se/hockersten/timed/vibration/main/MainFragment.java
@@ -17,14 +17,28 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 
+// TODO
+// set something different from "disabled" by default
+// add Swedish translation
+// add configuration of vibrations
+// beautify the UI more
+// add feature: active time measurement (aka "competition mode")
+
 public class MainFragment extends Fragment {
+	private static String COUNTING = "COUNTING";
+	private static String VIBRATE_ONCE_TASK = "VIBRATE_ONCE_TASK";
+	private static String VIBRATE_TWICE_TASK = "VIBRATE_TWICE_TASK";
+	
 	private boolean counting = false;
 	private PendingIntent vibrateOnceTask;
 	private PendingIntent vibrateTwiceTask;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		if (savedInstanceState == null) {
+		if (savedInstanceState != null) {
+			counting = savedInstanceState.getBoolean(COUNTING);
+			vibrateOnceTask = savedInstanceState.getParcelable(VIBRATE_ONCE_TASK);
+			vibrateTwiceTask = savedInstanceState.getParcelable(VIBRATE_TWICE_TASK);
 		}
 		
 		View v = inflater.inflate(R.layout.main_fragment, container, false);
@@ -47,7 +61,16 @@ public class MainFragment extends Fragment {
 		// Apply the adapter to the spinner
 		spinner2.setAdapter(adapter2);
 		
+		updateUI(v);
+		
 		return v;
+	}
+	
+	@Override
+	public void onSaveInstanceState(Bundle b) {
+		b.putBoolean(COUNTING, counting);
+		b.putParcelable(VIBRATE_ONCE_TASK, vibrateOnceTask);
+		b.putParcelable(VIBRATE_TWICE_TASK, vibrateTwiceTask);
 	}
 	
 	@Override
@@ -56,15 +79,12 @@ public class MainFragment extends Fragment {
 		
 		startBtn.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
-				Button self = (Button) v;
+				Spinner spinSingle = (Spinner) getActivity().findViewById(R.id.spinIntervalSingle);
+				Spinner spinDouble = (Spinner) getActivity().findViewById(R.id.spinIntervalDouble);
 				counting = !counting;
 
 				if (counting) {
-					self.setText(R.string.stop_counting);
-					Spinner spinSingle = (Spinner) getActivity().findViewById(R.id.spinIntervalSingle);
-					Spinner spinDouble = (Spinner) getActivity().findViewById(R.id.spinIntervalDouble);
-					spinSingle.setEnabled(false);
-					spinDouble.setEnabled(false);
+					updateUI(getActivity().findViewById(android.R.id.content));
 
 					Calendar nextApplicableMinuteSingle = Calendar.getInstance();
 					Calendar nextApplicableMinuteDouble = Calendar.getInstance();
@@ -95,12 +115,7 @@ public class MainFragment extends Fragment {
 					//alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 5000, delayDouble, vibrateTwiceTask);
 				}
 				else {
-					self.setText(R.string.start_counting);
-					Spinner spinSingle = (Spinner) getActivity().findViewById(R.id.spinIntervalSingle);
-					Spinner spinDouble = (Spinner) getActivity().findViewById(R.id.spinIntervalDouble);
-					spinSingle.setEnabled(true);
-					spinDouble.setEnabled(true);
-
+					updateUI(getActivity().findViewById(android.R.id.content));
 					AlarmManager alarmManager = (AlarmManager) getActivity().getSystemService(Context.ALARM_SERVICE);
 					alarmManager.cancel(vibrateOnceTask);
 					alarmManager.cancel(vibrateTwiceTask);
@@ -111,6 +126,20 @@ public class MainFragment extends Fragment {
 		});
 		
 		super.onResume();
+	}
+	
+	private void updateUI(View v) {
+		Button startBtn = (Button) v.findViewById(R.id.btnStart);
+		Spinner spinSingle = (Spinner) v.findViewById(R.id.spinIntervalSingle);
+		Spinner spinDouble = (Spinner) v.findViewById(R.id.spinIntervalDouble);
+		spinSingle.setEnabled(!counting);
+		spinDouble.setEnabled(!counting);
+		if (counting) {
+			startBtn.setText(R.string.stop_counting);
+		}
+		else {
+			startBtn.setText(R.string.start_counting);
+		}
 	}
 	
 	private int normalizedMinuteDelay(int currentMinute, int delay) {
